f77d88035a1c726e5ac19e5af646ae77d4d84d18
Shibin Tazhe Veettil <wolf@Shibins-Mini.attlocal.net>
2022-05-16 00:01:08 -0500

    client implementation: telegram only

--- telegram_reader.py
+++ telegram_reader.py
@@ -15,8 +15,6 @@ async def main():
     client = TelegramClient('session_name', credential.appId , credential.apiHash)
     await client.start()
 
-    print("client started")
-
     @client.on(events.NewMessage())
     async def handler(event):
         message = event.message
@@ -24,13 +22,13 @@ async def main():
         wrappers = Messages(channelId = str(message.peer_id.channel_id), messages = [wrapper])
         forward_setting, filteredMessages = SettingHelper.filterMessages(wrappers)
         if filteredMessages and len(filteredMessages.messages) > 0:
-            print(message)
             destId = forward_setting.desination.id
             entity = None
             if destId not in entities:
-                entity = entity = await client.get_entity(destId)
+                entity = await client.get_entity(int(destId))
                 entities[destId] = entity
-
+            else:
+                entity = entities[destId]
             await client.forward_messages(entity=entity, messages=message)
 
 
